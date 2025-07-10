e3259e60e8ee1bf73d0faaca9117fcf2a11f3a1d
Saeed Mohamed <sydalshghty15@gmail.com>
2025-07-10 14:31:23 +0300

    update-changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  base: '/My-Portofolio/',
+  plugins: [ 
     react(),
   ],
 })
